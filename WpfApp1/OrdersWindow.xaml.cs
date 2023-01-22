8dff102d25771eb7ae0dd9b3059d45544c86ac12
Camille-Guo <zibin_guo@hotmail.com>
2023-01-22 04:35:48 -0500

    Add Export to Excel funciont in OrderWindow

--- WpfApp1/OrdersWindow.xaml.cs
+++ WpfApp1/OrdersWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,40 +13,186 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using Window = Microsoft.Office.Interop.Excel.Window;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for OrdersWindow.xaml
     /// </summary>
-    public partial class OrdersWindow : Window
+    public partial class OrdersWindow : System.Windows.Window
     {
+        string fileName = "D:\\MyExcel.xlsx";
         public OrdersWindow()
         {
             InitializeComponent();
+            try
+            {
+                Globals.dbContext = new FurnitureDbContext(); // Exceptions
+                LvOrders.ItemsSource = Globals.dbContext.Orders.ToList(); // equivalent of SELECT * FROM people
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(this, "Error reading from database\n" + ex.Message, "Fatal error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                // Close();
+                Environment.Exit(1);
+            }
         }
+
 
         private void BtnSaveToExcel_Click(object sender, RoutedEventArgs e)
         {
+            List<Order> oList = Globals.dbContext.Orders.ToList();
 
-        }
+            if (oList.Count == 0)
+            {
+                MessageBox.Show(this, "No selectd items!", "File export error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
-        private void BtnSaveToPdf_Click(object sender, RoutedEventArgs e)
-        {
+            try
+            {
+                //Create an object of Excel application
+                Microsoft.Office.Interop.Excel.Application excelAPP = null;
+                Microsoft.Office.Interop.Excel.Workbook WB = null;
+                Microsoft.Office.Interop.Excel.Worksheet WS = null;
+                Microsoft.Office.Interop.Excel.Range Range = null;
 
+                //Create an object of Workbook
+                excelAPP = new Microsoft.Office.Interop.Excel.Application();
+                WB = excelAPP.Workbooks.Add(fileName);
+                //WB = OpenExcelApp("D:\\MyExcel.xlsx", 1);
+                //WB = excelAPP.Workbooks.Add("D:\\MyExcel.xlsx");
+                WS = (Microsoft.Office.Interop.Excel.Worksheet)WB.Sheets[1];
+                
+                //CreateHeader;
+                WS.Cells[1, 1] = "OrderId";
+                WS.Cells[1, 2] = "CustomerId";
+                WS.Cells[1, 3] = "UserId";
+                WS.Cells[1, 4] = "OrderDate";
+                WS.Cells[1, 5] = "TotalPaied";
+
+                //InsertData;
+                int ind = 2;
+                foreach (Order order in oList)
+                {
+                    //DataRow DR = DRV.Row;
+                    //for (int ind1 = 0; ind1 < oList.Count; ind1++)
+                    //{
+                    WS.Cells[1][ind] = order.OrderId;
+                    WS.Cells[2][ind] = order.CustomerId;
+                    WS.Cells[3][ind] = order.UserId;
+                    WS.Cells[4][ind] = order.OrderDate;
+                    WS.Cells[5][ind] = order.TotalPaied;
+                    //}
+                    ind++;
+                }
+
+                //CloseExcelApp();
+                if (excelAPP.ActiveWorkbook != null)
+                    excelAPP.ActiveWorkbook.Save();
+                if (excelAPP != null)
+                {
+                    if (WB != null)
+                    {
+                        if (WS != null)
+                            Marshal.ReleaseComObject(WS);
+                        WB.Close(false, Type.Missing, Type.Missing);
+                        Marshal.ReleaseComObject(WB);
+                    }
+                    excelAPP.Quit();
+                    Marshal.ReleaseComObject(excelAPP);
+                }
+                MessageBox.Show(this, "Export success!\n"+fileName, "Writing Confirmation", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+
+            catch (IOException ex )
+            {
+                MessageBox.Show(this, "Failed to Export to Excel File\n" + ex.Message, "Inner error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            //void OpenExcelApp(string Location, int workSheet)
+            //{
+            //    //WB = excelAPP.Workbooks.Open(Location);
+            //    WB = excelAPP.Workbooks.Add(Location);
+            //    WS = (Microsoft.Office.Interop.Excel.Worksheet)WB.Sheets[workSheet];
+            //    //return WS;
+            //}
+            //void CreateHeader()
+            //{
+            //    //int ind = 1;
+            //    //foreach (object ob in this.dgvFields.Columns.Select(cs => cs.Header).ToList())
+            //    //{
+            //    //    this.WS.Cells[1, ind] = ob.ToStrihng();
+            //    //    ind++;
+            //    //}
+            //    WS.Cells[1, 1] = "OrderId";
+            //    WS.Cells[1, 2] = "CustomerId";
+            //    WS.Cells[1, 3] = "UserId";
+            //    WS.Cells[1, 4] = "OrderDate";
+            //    WS.Cells[1, 5] = "TotalPaied";
+            //}
+            //void InsertData()
+            //{
+            //    int ind = 2;
+            //    foreach (Order order in oList)
+            //    {
+            //        //DataRow DR = DRV.Row;
+            //        //for (int ind1 = 0; ind1 < oList.Count; ind1++)
+            //        //{
+            //        WS.Cells[ind][1] = order.OrderId;
+            //        WS.Cells[ind][2] = order.CustomerId;
+            //        WS.Cells[ind][3] = order.UserId;
+            //        WS.Cells[ind][4] = order.OrderDate;
+            //        WS.Cells[ind][5] = order.TotalPaied;
+            //        //}
+            //        ind++;
+            //    }
+            //}
+            //void CloseExcelApp()
+            //{
+            //    if (excelAPP.ActiveWorkbook != null)
+            //        excelAPP.ActiveWorkbook.Save();
+            //    if (excelAPP != null)
+            //    {
+            //        if (WB != null)
+            //        {
+            //            if (WS != null)
+            //                Marshal.ReleaseComObject(WS);
+            //            WB.Close(false, Type.Missing, Type.Missing);
+            //            Marshal.ReleaseComObject(WB);
+            //        }
+            //        excelAPP.Quit();
+            //        Marshal.ReleaseComObject(excelAPP);
+            //    }
+            //}
         }
 
         private void LvOrders_SelectionChanged(object sender, RoutedEventArgs e)
         {
+            Order currSelOrder = LvOrders.SelectedItem as Order;
+            if (currSelOrder == null) return;
 
+            var currentOrderItem = Globals.dbContext.OrderItems.Where(o => o.OrderId == currSelOrder.OrderId).ToList();
+
+            if (currentOrderItem == null) return;
+            OrderDetailDialog orderDetailDialog = new OrderDetailDialog();
+            orderDetailDialog.Owner = this;
+            orderDetailDialog.LvOrderDetail.ItemsSource = currentOrderItem;
+            orderDetailDialog.LblTotal.Content = currSelOrder.TotalPaied.ToString();
+            orderDetailDialog.ShowDialog();
         }
 
-        private void LvOrderDetail_SelectionChanged(object sender, RoutedEventArgs e)
-        {
 
-        }
+
     }
+
 }
