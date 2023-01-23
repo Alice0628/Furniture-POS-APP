9138ef9ed6ef57827bcfdf90fbaba5cd3121b13e
Qichen Zhu <109364886+Electivire9@users.noreply.github.com>
2023-01-22 22:28:22 -0500

    0122

--- FurnitureMSTest/CheckoutWindowTests.cs
+++ FurnitureMSTest/CheckoutWindowTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WpfApp1.Tests
+namespace FurnitureMSTest
 {
     [TestClass()]
     public class CheckoutWindowTests
