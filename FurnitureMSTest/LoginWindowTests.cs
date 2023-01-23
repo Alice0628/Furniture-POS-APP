7d68e0682e6d86653ab4a44759f41a1fffc7104b
Xiao Zhang <zxiao0628@gmail.com>
2023-01-22 22:24:08 -0500

    add unit test

---
+++ FurnitureMSTest/LoginWindowTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Tests
+{
+    [TestClass()]
+    public class LoginWindowTests
+    {
+        [TestMethod()]
+        public void VerifyIdentityTest()
+        {
+            Assert.Fail(); string email = "john@gmail.com";
+            string pass = "123";
+
+            WpfApp1.LoginWindow lgWindow = new WpfApp1.LoginWindow();
+            bool isFound = lgWindow.VerifyIdentity(email, pass);
+
+            Assert.IsTrue(isFound);
+
+           
+        }
+    }
+}
